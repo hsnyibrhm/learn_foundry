0ab6cee3d30b5bdcddeb1a835db314921a7d26ae
hasany ibrohim <hasanyibrohim546@gmail.com>
2026-04-20 20:24:31 +0700

    tes

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,13 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-contract SimpleStorage {
-    uint256 private storedData;
 
-    function set(uint256 x) public {
-        storedData = x;
+contract SimpleStorage {
+    uint256 public noFav;
+    //uint256 [] listnofav;
+
+    struct Orang {
+        uint256 NoFav;
+        string nama;
     }
 
-    function get() public view returns (uint256) {
-        return storedData;
+    Orang[] public listOrang;
+
+    mapping(string => uint256) public namaToNoFav;
+
+    function store(uint256 _noFav) public virtual {
+        noFav = _noFav;
+    }
+
+    function retrive() public view returns (uint256) {
+        return noFav;
+    }
+
+    // calldata memory, storage
+    function addOrang(string memory _nama, uint256 _nofav) public {
+        listOrang.push(Orang(_nofav, _nama));
+        namaToNoFav[_nama] = _nofav;
     }
 }
